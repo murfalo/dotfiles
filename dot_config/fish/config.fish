a833784178cf9ce265aebecc21d973e4824c47a2
Murphy Angelo <git@murfalo.com>
2024-07-16 12:52:40 -0400

    Minor fixes

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -110,11 +110,10 @@ end
 function fish_greeting
 	echo
 	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
 	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
 
 	set_color normal
-	echo -e " \e[1mTODOs\e[0;32m"
+	echo -e " \e[1mTODOs:\e[0;32m"
 	echo
 
 	if test -s ~/todo
